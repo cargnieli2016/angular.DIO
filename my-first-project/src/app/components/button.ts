da2617818856e715fe29420906bc656ea2e7fcf3
Cargnieli2016 <cargnieli2016@gmail.com>
2025-08-23 08:06:22 -0300

    Criando componentes reutilizaveis

---
+++ my-first-project/src/app/components/button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-button',
+    templateUrl: './button.html',
+    styleUrl: './button.css'
+})
+
+export class Button{
+
+}
